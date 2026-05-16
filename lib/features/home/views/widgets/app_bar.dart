85acbd4a0a84b596402570b4d7dd29c8ec6d9f15
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-05-16 17:40:54 +0600

    Add AppBar

---
+++ lib/features/home/views/widgets/app_bar.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MyAppBar extends GetView {
+  const MyAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            // profile Image
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.yellow[700],
+                  ),
+                ),
+                Icon(
+                  CupertinoIcons.person_fill,
+                  size: 25,
+                  color: Theme.of(context).colorScheme.onSurface.withAlpha(150),
+                ),
+              ],
+            ),
+            SizedBox(width: 8),
+            // welcome and name
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome!",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                ),
+                Text(
+                  "Bikram Nath",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+
+        // settings button
+        IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.settings)),
+      ],
+    );
+  }
+}
