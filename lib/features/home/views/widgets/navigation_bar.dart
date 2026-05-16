a8b8f0478a0b3ab7b707056c3e830683e9211566
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-05-16 17:53:48 +0600

    Add Navigation bar

---
+++ lib/features/home/views/widgets/navigation_bar.dart
@@ -0,0 +1,38 @@
+import 'package:expense_tracker/features/home/view_model/home_controller.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class AppNavigationBar extends GetView<HomeController> {
+  const AppNavigationBar({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+      child: Obx(
+        ()=> BottomNavigationBar(
+          currentIndex:controller.currentPage.value,
+          onTap: (value) {
+            controller.currentPage.value = value;
+           
+          },
+        
+          backgroundColor: Colors.white,
+          
+          showUnselectedLabels: false,
+          elevation: 3,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.profile_circled),
+              label: "Profile",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
