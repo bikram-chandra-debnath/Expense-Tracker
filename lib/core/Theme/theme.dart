bb922c5a30aa980a2a5cd33e3ccea8c1d6ad3f9f
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-05-16 15:29:42 +0600

    Add App Theme

---
+++ lib/core/Theme/theme.dart
@@ -0,0 +1,29 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+
+  static final lightTheme = ThemeData(       
+        colorScheme: const ColorScheme.light(
+          surface: Colors.grey,
+          onSurface: Colors.black,
+          primary: Color(0xff00B2E7),
+          secondary: Color(0xffE064f7),
+          tertiary: Color(0xffff8d6c),
+          outline: Colors.grey,
+        )
+      );
+  static final darkTheme = ThemeData(       
+        colorScheme: const ColorScheme.light(
+          surface: Colors.grey,
+          onSurface: Colors.black,
+          primary: Color(0xff00B2E7),
+          secondary: Color(0xffE064f7),
+          tertiary: Color(0xffff8d6c),
+          outline: Colors.grey,
+        )
+      );
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,29 +1,19 @@
 
 
-//import 'package:expense_tracker/screen/home/views/home_screen.dart';
+import 'package:expense_tracker/core/Theme/theme.dart';
+import 'package:expense_tracker/features/home/views/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        colorScheme: ColorScheme.light(
-          background: Colors.grey.shade100,
-          onBackground: Colors.black,
-          primary: Color(0xff00B2E7),
-          secondary: Color(0xffE064f7),
-          tertiary: Color(0xffff8d6c),
-          outline: Colors.grey.shade400,
-        )
-      ),
-     //home: HomeScreen(),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+     home: HomeScreen(),
     );
   }
 }
