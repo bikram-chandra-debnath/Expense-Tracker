456950d311005b54e512d1838718f183381eac73
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-05-16 15:37:21 +0600

    Add Getx

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -3,17 +3,22 @@
 import 'package:expense_tracker/core/Theme/theme.dart';
 import 'package:expense_tracker/features/home/views/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-     home: HomeScreen(),
+     initialRoute: "/home",
+
+     getPages: [
+      GetPage(name: "/home", page: ()=> HomeScreen() )
+     ],
     );
   }
 }
