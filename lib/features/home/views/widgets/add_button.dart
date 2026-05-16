c62ba2f9a5654984ac1337bdaddc8c5b143a0f48
bikram-chandra-debnath <bikramchandradebnath02@gmail.com>
2026-05-16 20:58:48 +0600

    add expenses screen route

--- lib/features/home/views/widgets/add_button.dart
+++ lib/features/home/views/widgets/add_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FloatingAddButton extends StatelessWidget {
   const FloatingAddButton({
@@ -9,7 +10,7 @@ class FloatingAddButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: ()=> Get.toNamed("/addExpenses"),
       shape: const CircleBorder(),
       child: Container(
         height: 60,
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,7 +1,9 @@
+import 'package:expense_tracker/features/add%20expenses/view/screen/add_expenses_screen.dart';
 import 'package:expense_tracker/features/home/bindings/home_binding.dart';
 import 'package:expense_tracker/features/home/views/screen/home_screen.dart';
 import 'package:get/get.dart';
 
 List <GetPage> routingPages=  [
-      GetPage(name: "/home", page: ()=> HomeScreen(), bindings: [HomeBinding()])
+      GetPage(name: "/home", page: ()=> HomeScreen(), bindings: [HomeBinding()]),
+      GetPage(name: "/addExpenses", page: ()=> AddExpensesScreen(), ),
      ];
